ad1d88f8658c50f819ca3610c4e8b8a1d117b214
thomas-nynas <joseph.fennell.dev@gmail.com>
2025-04-23 18:05:57 -0400

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="https://biren-patel.vercel.app/favicon.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
